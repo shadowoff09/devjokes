ec47e4bc95976a86ab419662c6ad79ffc34f3d23
Diogo Gaspar <diogogaspar931@gmail.com>
2023-03-24 02:11:01 +0000

    Refactor code to include functionality for randomly selecting and displaying images in addition to jokes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="absolute bottom-0 w-full h-16">
+    <div className="fixed bottom-0 w-full h-16">
       <div className="h-full w-full flex justify-center items-center">
         <h1 className="text-normal sm:text-md md:text-lg lg:text-xl font-bold text-white">
           Made with ❤️ by {` `}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Footer from "@/components/Footer";
 import Jokes from "@/components/Jokes";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import Navbar from "@/components/Navbar";
-import axios from "axios";
+import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
   const [joke, setJoke] = useState({question: "", punchline: ""}); // Set initial state
+  const [image, setImage] = useState(""); // Set initial state
   const [isLoading, setLoading] = useState(false); // Set initial state
 
   async function getJoke() {
@@ -22,28 +23,56 @@ export default function Home() {
     setLoading(false); // Hide spinner
   }
 
+  // async function getImage() {
+  //   setLoading(true); // Show spinner
+  //   const res = await fetch(
+  //     `/api/random`,
+  //     { next: { revalidate: 0 } }
+  //   );
+  //   const data = await res.json();
+  //   console.log(data.image);
+  //   setImage(data.image);
+  // }
+
   return (
     <>
+      <Head>
+        <title>Dev Jokes</title>
+      </Head>
       {/* Navbar */}
       <Navbar />
       {/* Jokes */}
       <div className="flex flex-col mt-60 ">
         <div className="text-center">
-          <Button text="Get a Joke 😂" onClick={getJoke} />
+          <Button
+            text="Get a Joke 😂"
+            onClick={() => {
+              getJoke();
+              // getImage();
+            }}
+          />
         </div>
         <div className="flex justify-center items-center">
           {isLoading ? (
-              <LoadingSpinner />
+            <LoadingSpinner />
+          ) : // check if joke is empty}
+          joke.question ? (
+            <Jokes question={joke.question} punchline={joke.punchline} />
           ) : (
-            // check if joke is empty}
-            joke.question ? (
-              <Jokes question={joke.question} punchline={joke.punchline} />
-            ) : (
-              <h1 className="text-2xl font-bold text-white mt-5 mx-5">Click above to get a joke!</h1>
-            )
-            
+            <h1 className="text-2xl font-bold text-white mt-5 mx-5">
+              Click above to get a joke!
+            </h1>
           )}
         </div>
+        {/* <div className="flex justify-center items-center mt-5">
+          {image ? (
+            <Image src={image} alt="Random Image" width={300} height={300} />
+          ) : (
+            <h1 className="text-2xl font-bold text-white mt-5 mx-5">
+              No Image
+            </h1>
+          )}
+        </div> */}
       </div>
       {/* Footer */}
       <Footer />
